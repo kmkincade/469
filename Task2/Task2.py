1834d242bff2a08b530f6b2b3a4c9587ff19ba42
kmkincade <kmkincad@asu.edu>
2017-04-27 20:55:07 -0700

    Finished Up Task 2

--- Task2/Task2.py
+++ Task2/Task2.py
@@ -1,11 +1,14 @@
 import hashlib
+from Task2PartC import partitions
+import binascii
+
 
 def main():
-    #getting path to a RAW image file (requirement a)
+    # getting path to a RAW image file (requirement a)
     fileString = input("Enter a path to a RAW image file: ")
     newfileString = fileString.split('\\')
     fileName = newfileString[newfileString.__len__() - 1]
-    #now calculate checksums (requirement b)
+    # now calculate checksums (requirement b)
     BUF_SIZE = 65536
     md5 = hashlib.md5()
     sha1 = hashlib.sha1()
@@ -21,6 +24,8 @@ def main():
     fileMD5.write("MD5: {0}".format(md5.hexdigest()))
     fileSHA1 = open("SHA1-" + fileName.split(".")[0] + ".txt", "w+")
     fileSHA1.write("SHA1: {0}".format(sha1.hexdigest()))
-    
+    partitions(fileString)
+
+
 if __name__ == "__main__":
     main()
--- Task2/Task2PartC.py
+++ Task2/Task2PartC.py
@@ -39,10 +39,10 @@ def get_partition_tables(f):
     f.seek(446)
     partition_tables = f.read(128)
     partition_tables = binascii.hexlify(partition_tables) # this is formatting the tables in an easy to use manner
-    partition_tables = [partition_tables[i*32:i*32+32] for i in xrange(4)]
+    partition_tables = [partition_tables[i*32:i*32+32] for i in range(4)]
     partition_tables = map(lambda x: hex(int(x, 16))[2:].replace('L', ''), partition_tables)
     partition_tables = map(lambda x: '0'*(32-len(x)) + x, partition_tables)
-    partition_tables = map(lambda x: [x[i*2:i*2+2] for i in xrange(16)], partition_tables) # Now looks like [['00', '20', '21', '00', '0b', '55', '11', '04', '00', '08', '00', '00', '00', '08', '01', '00']]
+    partition_tables = map(lambda x: [x[i*2:i*2+2] for i in range(16)], partition_tables) # Now looks like [['00', '20', '21', '00', '0b', '55', '11', '04', '00', '08', '00', '00', '00', '08', '01', '00']]
     return partition_tables
 
 def print_partition_info(f):
@@ -58,6 +58,51 @@ def print_partition_info(f):
         size = int(''.join(reversed(partition[12:])), 16)
 
         print(print_string.format(hex=hex_val, name=name, start_sector=start_sector, size=size))
+        if name.__contains__("FAT"):
+                # 512 bytes in a sector
+                f.seek(start_sector * 512)
+                vbr = f.read(128)
+                vbr = binascii.hexlify(vbr)  # this is formatting the tables in an easy to use manner
+                vbr = [vbr[i * 32:i * 32 + 32] for i in range(4)]
+                vbr = map(lambda x: hex(int(x, 16))[2:].replace('L', ''), vbr)
+                vbr = map(lambda x: '0' * (32 - len(x)) + x, vbr)
+                vbr = map(lambda x: [x[i * 2:i * 2 + 2] for i in range(16)], vbr)
+                i = 0
+                reserved_start = 0
+                reserved_end = 0
+                fat_count = 0
+                fat_size = 0
+                ending_sector = 0
+                cluster_2_start = 0
+                for information in vbr:
+                    #print(information)
+                    i+=1
+                    if i==1:
+                        reserved_start += start_sector
+                        reserved_end += reserved_start + int(''.join(reversed(information[14:16])), 16)
+                        print("Reserved Area: Start Sector: " + str(reserved_start) + " Ending Sector: " + str(reserved_end) + " " + str(reserved_end - reserved_start) + " sectors")
+                        sectors_per_cluster = int(''.join(information[13:14]), 16)
+                        print("Sectors per cluster: " + str(sectors_per_cluster) + " sectors")
+                    elif i == 2:
+                        fat_count += int(''.join(information[0:1]), 16)
+                        if name.__contains__("16-bit"):
+                            fat_size += int(''.join(reversed(information[6:8])), 16)
+                            cluster_2_start += int(''.join(reversed(information[1:3])), 16)
+                    elif i == 3:
+                        if name.__contains__("32-bit"):
+                            fat_size += int(''.join(reversed(information[4:8])), 16)
+                        ending_sector += fat_count*fat_size + reserved_end + 1
+                        print("FAT area: Start sector: " + str(reserved_end + 1) + " Ending sector: " + str(ending_sector))
+                        print("# of FATs: " + str(fat_count))
+                        print("Size of each FAT: " + str(fat_size))
+                        if name.__contains__("32-bit"):
+                            print("The first sector of cluster 2: " + str(ending_sector + 1) + "\n")
+                        else:
+                            cluster_2_start += ending_sector + 1
+                            print("The first sector of cluster 2: "+ str(cluster_2_start) + "\n")
+
+
+
 
 def get_filesystem_info(f, start_sector):
     f.seek(start_sector)
